6e4610495564b6e8fd18d5483c22d0ad1d8bdcfc
alechp <me@alechp.com>
2019-03-26 10:40:18 -0700

    Added repogen config & LAF projects

---
+++ .repogen.js
@@ -0,0 +1,10 @@
+const config = {
+  provider: "alechp",
+  repospacePath: __dirname,
+  repositories: [
+    { servexyz: "lingo-asset-fetcher-lib" },
+    { servexyz: "lingo-asset-fetcher-cli" }
+  ]
+};
+
+module.exports = { config };
